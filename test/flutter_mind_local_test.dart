4d78426f23fda37fdf97cd13b26ba5487f290c28
MohamedOsama26 <mohamed.99.kandel@gmail.com>
2026-06-13 15:36:51 +0300

    Initial scaffold from flutter create --template=package_ffi

---
+++ test/flutter_mind_local_test.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+
+import 'package:flutter_mind_local/flutter_mind_local.dart';
+
+void main() {
+  test('invoke native function', () {
+    expect(sum(24, 18), 42);
+  });
+
+  test('invoke async native callback', () async {
+    expect(await sumAsync(24, 18), 42);
+  });
+}
